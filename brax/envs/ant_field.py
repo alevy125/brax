4d0a1d8cac457d1f6d844e3f97af33b9d96d35e8
Andrew Levy <alevy125@gmail.com>
2022-11-01 11:02:21 -0400

    Update ant_field.py

--- brax/envs/ant_field.py
+++ brax/envs/ant_field.py
@@ -243,7 +243,7 @@ _SYSTEM_CONFIG = """
   bodies {
     name: "North_Wall"
     colliders {
-      box { halfsize { x: 5.0 y: 1.2 z: 0.75} }
+      box { halfsize { x: 4.9 y: 1.2 z: 0.75} }
       position {x: 0.0 y: 3.7 z: 0.75}
     }
     frozen { all: true }
@@ -259,7 +259,7 @@ _SYSTEM_CONFIG = """
   bodies {
     name: "South_Wall"
     colliders {
-      box { halfsize { x: 5.0 y: 1.2 z: 0.75} }
+      box { halfsize { x: 4.9 y: 1.2 z: 0.75} }
       position {x: 0.0 y: -3.7 z: 0.75}
     }
     frozen { all: true }
